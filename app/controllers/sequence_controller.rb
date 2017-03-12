ecd9bc772a7efae51107785d7fb4383a5a6d9eb5
GGCrew <R_Connelie@hotmail.com>
2017-03-11 22:49:13 -0800

    Add more code to create lengthy sequence data structure

--- app/controllers/sequence_controller.rb
+++ app/controllers/sequence_controller.rb
@@ -6,18 +6,27 @@ class SequenceController < ApplicationController
 
 	CCRS = [
 		{
-			unit: 1,
+			unit: '01',
 			inactive: [6,12,18,24,31,36,43,49]
 		},
 		{
-			unit: 11,
+			unit: '11',
 			inactive: [6,13,25,43,48]
 		},
 		{
-			unit: 21,
+			unit: '21',
 			inactive: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,36,42,48]
 		}
 	]
+
+	COLORS = [
+		{ name: 'red',   initial: 'R', value: 255 },
+		{ name: 'green', initial: 'G', value: 65280 },
+		{ name: 'blue',  initial: 'B', value: 16711680 }
+	]
+
+	CENTISECONDS = 400
+	CENTISECONDINTERVAL = 10
 
 =begin
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
@@ -89,7 +98,105 @@ class SequenceController < ApplicationController
 	def index
 		channels = []
 		CCRS.each do |ccr|
+=begin
+			<channel name="CCD 21 p50 - 18 (R)" color="255" centiseconds="400" deviceType="LOR" unit="33" circuit="148" savedIndex="0"/>
+			<channel name="CCD 21 p50 - 18 (G)" color="65280" centiseconds="400" deviceType="LOR" unit="33" circuit="149" savedIndex="1">
+				<effect type="intensity" startCentisecond="0" endCentisecond="400" intensity="50"/>
+			</channel>
+			<channel name="CCD 21 p50 - 18 (B)" color="16711680" centiseconds="400" deviceType="LOR" unit="33" circuit="150" savedIndex="2"/>
+			<rgbChannel totalCentiseconds="400" name="CCD 21 p50 - 18" savedIndex="3">
+				<channels>
+					<channel savedIndex="0"/>
+					<channel savedIndex="1"/>
+					<channel savedIndex="2"/>
+				</channels>
+			</rgbChannel>
+=end
+=begin
+			{
+				channels: [
+					{
+						attributes: {name: '', color: '', centiseconds: '', deviceType: '', unit: '', circuit: '', savedIndex:''},
+						effects: [
+							{
+								attributes: {type: '', startCentisecond: '', endCentisecond: '', startIntensity: '', endIntensity: ''}
+							}
+						]
+					}
+				],
+				rgbChannel: {
+					attributes: {totalCentiseconds: '', name: '', savedIndex: ''},
+					channels: [
+						{
+							attributes: {savedIndex: ''}
+						}
+					]
+				}
+			}
+=end
+			current_index = 0
 			
+			(1..50).each do |pixel_index|
+				color_indices = []
+				(0..2).each do |color_index|
+					#color = (2**(8*(color_index+1))) - (2**(8*color_index))
+					color = COLORS[color_index]
+				
+					# RGB channels
+					attributes = {}
+					attributes.merge!({name: "CCR #{ccr[:unit]} p#{pixel_index} (#{color[:initial]})"})
+					attributes.merge!({color: color[:value]})
+					attributes.merge!({centiseconds: CENTISECONDS})
+					attributes.merge!({deviceType: 'LOR'})
+					attributes.merge!({unit: ccr[:unit].to_s.to_i(16)}) # convert from hex to decimal
+					attributes.merge!({circuit: (((pixel_index - 1) * 4) + color_index)})
+					attributes.merge!({savedIndex: current_index})
+
+					if color_index == 1 # green
+						effects = []
+						(0..(CENTISECONDS - CENTISECONDINTERVAL)).step(CENTISECONDINTERVAL) do |centisecond|
+							# TODO: generate random intensity values
+							effects << {
+								attributes: {
+									type: 'intensity',
+									startCentisecond: centisecond,
+									endCentisecond: (centisecond + CENTISECONDINTERVAL),
+									startIntensity: 1,
+									endIntensity: 50
+								}
+							}
+						end
+					else
+						effects = nil
+					end
+
+					current_index += 1
+				end
+			end
+
+
+=begin
+			<channel name="CCD 21 - LR" color="12615744" centiseconds="400" deviceType="LOR" unit="33" circuit="151" priority="67108864" savedIndex="200"/>
+			<channel name="CCD 21 - MM" color="12615744" centiseconds="400" deviceType="LOR" unit="33" circuit="152" priority="67108864" savedIndex="201"/>
+			<channel name="CCD 21 - MS" color="12615744" centiseconds="400" deviceType="LOR" unit="33" circuit="153" priority="67108864" savedIndex="202"/>
+			<channel name="CCD 21 - ME" color="12615744" centiseconds="400" deviceType="LOR" unit="33" circuit="154" priority="67108864" savedIndex="203"/>
+			<channel name="CCD 21 - CM" color="12615744" centiseconds="400" deviceType="LOR" unit="33" circuit="155" priority="67108864" savedIndex="204"/>
+			<channel name="CCD 21 - CS" color="12615744" centiseconds="400" deviceType="LOR" unit="33" circuit="156" priority="67108864" savedIndex="205"/>
+			<channel name="CCD 21 - CI" color="12615744" centiseconds="400" deviceType="LOR" unit="33" circuit="157" priority="67108864" savedIndex="206"/>
+			<cosmicColorDevice totalCentiseconds="400" name="CCD 21" savedIndex="207">
+				<channelGroups>
+					<channelGroup savedIndex="3"/>
+					<channelGroup savedIndex="7"/>
+					<channelGroup savedIndex="200"/>
+					<channelGroup savedIndex="201"/>
+					<channelGroup savedIndex="202"/>
+					<channelGroup savedIndex="203"/>
+					<channelGroup savedIndex="204"/>
+					<channelGroup savedIndex="205"/>
+					<channelGroup savedIndex="206"/>
+				</channelGroups>
+			</cosmicColorDevice>
+=end
 		end
 
 		@sequence = {
